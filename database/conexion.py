307d78f14ee9158a7136b53ce7899cdc04c31127
Santiago Padierna <soypadierna@gmail.com>
2026-09-12 21:57:32 -0500

    Add conexion a base de datos con psycopg2 y dotenv

---
+++ database/conexion.py
@@ -0,0 +1,22 @@
+import os
+import psycopg2
+from psycopg2 import OperationalError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_connection():
+    try:
+        conexion = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+        )
+        return conexion
+    except OperationalError as e:
+        print(f"Error al conectar a la base de datos: {e}")
+        return None
+    
